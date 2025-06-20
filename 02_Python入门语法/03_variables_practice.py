ca65de42dacf29836837954abd5bde7a114b245f
sekitonu <jb18880@163.com>
2025-06-20 15:02:01 +0800

    新增变量练习的脚本

---
+++ 02_Python入门语法/03_variables_practice.py
@@ -0,0 +1,22 @@
+"""
+这是变量这一小节的练习题答案
+
+"""
+
+
+# 钱包初始余额为50
+money = 50
+# 输入当前钱包余额
+print("当前钱包余额：", money, "元")
+
+# 买了一个冰淇淋花费10元
+ice_cream = 10
+print("购买冰淇淋,花费了", ice_cream, "元")
+
+# 买了一杯可乐，花费了5元
+coca_cola = 5
+print("购买可口可乐，花费了", coca_cola, "元")
+
+# 输出余额
+print("最终，钱包剩余：", money - ice_cream - coca_cola, "元")
+
